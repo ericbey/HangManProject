e1e37cb9a133e822eb656b2441b5a116f11a261c
Cine Student <Cine Student@CINELAB12>
2024-01-31 20:48:41 -0800

    Add project files.
    Created project file/repo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangManProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hangman");
+            Console.WriteLine("The rules are simple. Guess the word, or the man will hang.");
+            
+            //creating an array of words that will be called for the game
+            string[] words = new string[] {};
+        }
+    }
+}
